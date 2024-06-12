2bfb818aa561f0c0a22ee7aa23e506f6e78d22dc
Sazeebul Bashar <sazeebul@gmail.com>
2024-06-12 13:16:38 +0600

    css added, some font changed

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -5,13 +5,33 @@ function Note({ note, onDelete }) {
     const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
 
     return (
-        <div className="note-container">
-            <p className="note-title">{note.title}</p>
-            <p className="note-content">{note.content}</p>
-            <p className="note-date">{formattedDate}</p>
-            <button className="delete-button" onClick={() => onDelete(note.id)}>
-                Delete
-            </button>
+        // <div className="note-container m-2 w-30">
+        //     <p className="note-title">{note.title}</p>
+        //     <p className="note-content">{note.content}</p>
+        //     <p className="note-date">{formattedDate}</p>
+        //     <button className="delete-button" onClick={() => onDelete(note.id)}>
+        //         Delete
+        //     </button>
+        // </div>
+        <div className="col">
+        <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">{note.title}</h5>
+                <hr />
+                <p className="card-text">
+                  {note.content.slice(0,80)}
+                </p>
+              </div>
+              <div className="d-flex justify-content-between">
+              <button className="delete-button" onClick={() => onDelete(note.id)}>
+              <i className="bi bi-trash-fill"></i>
+             </button>
+             <button className="delete-button">
+             <i className="bi bi-pen-fill"></i>
+             </button>
+              </div>
+              
+        </div>
         </div>
     );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,85 +1,106 @@
 import { useState, useEffect } from "react";
 import api from "../api";
-import Note from "../components/Note"
-import "../styles/Home.css"
+import Note from "../components/Note";
+import "../styles/Home.css";
+import "../styles/Form.css";
 
 function Home() {
-    const [notes, setNotes] = useState([]);
-    const [content, setContent] = useState("");
-    const [title, setTitle] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted!");
+        else alert("Failed to delete note.");
         getNotes();
-    }, []);
+      })
+      .catch((error) => alert(error));
+  };
 
-    const getNotes = () => {
-        api
-            .get("/api/notes/")
-            .then((res) => res.data)
-            .then((data) => {
-                setNotes(data);
-                console.log(data);
-            })
-            .catch((err) => alert(err));
-    };
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note created!");
+        else alert("Failed to make note.");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
 
-    const deleteNote = (id) => {
-        api
-            .delete(`/api/notes/${id}/`)
-            .then((res) => {
-                if (res.status === 204) alert("Note deleted!");
-                else alert("Failed to delete note.");
-                getNotes();
-            })
-            .catch((error) => alert(error));
-    };
+  return (
+    <div className="row m-5">
+      <div className="col-3">
+        <h2 className="heading">Create a Note</h2>
+        <form onSubmit={createNote} className="my-5">
+          
+          <br />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            placeholder="Title"
+          />
+         
+          <br />
+          <textarea
+            id="content"
+            name="content"
+            required
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Write something..."
+          ></textarea>
+          <br />
+          <input className="form-button" type="submit" value="Create"></input>
+        </form>
+      </div>
+      <div className="col-9 ">
+        <h2 className="heading">Notes</h2>
+        <hr />
 
-    const createNote = (e) => {
-        e.preventDefault();
-        api
-            .post("/api/notes/", { content, title })
-            .then((res) => {
-                if (res.status === 201) alert("Note created!");
-                else alert("Failed to make note.");
-                getNotes();
-            })
-            .catch((err) => alert(err));
-    };
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          
+            
+          {
+            (!notes || notes.length === 0) && <p>No notes found.</p> || notes &&
+            notes.map((note) => (
+            <Note note={note} onDelete={deleteNote} key={note.id} />
 
-    return (
-        <div>
-            <div>
-                <h2>Notes</h2>
-                {notes.map((note) => (
-                    <Note note={note} onDelete={deleteNote} key={note.id} />
-                ))}
-            </div>
-            <h2>Create a Note</h2>
-            <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <label htmlFor="content">Content:</label>
-                <br />
-                <textarea
-                    id="content"
-                    name="content"
-                    required
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                ></textarea>
-                <br />
-                <input type="submit" value="Submit"></input>
-            </form>
+          ))} 
+
+          </div>
         </div>
-    );
+
+        
+      </div>
+    
+  );
 }
 
 export default Home;
+
+
+
